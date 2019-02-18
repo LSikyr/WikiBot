ea9912a81d70481ad7668d1eeaa0e6a7d81f9a06
LSikyr <46223477+LSikyr@users.noreply.github.com>
2019-02-18 12:04:24 +0200

    WikiBot v1.0

---
+++ bot.py
@@ -0,0 +1,40 @@
+from telegram.ext import Updater
+from telegram.ext import MessageHandler, Filters
+import wikipedia as wiki
+
+
+TOKEN = "719781449:AAFPwK2uOz_OEWQCOFVaSAtLMUGphwUCVuk"
+
+updater = Updater(TOKEN)  #Bot Token is given by BotFather on Telegram while creating bot.
+
+
+def get_wiki(word):
+    language_list = ["java", "js", "c#"]
+    try:
+        if word in language_list:
+            return "Python is better!"
+        elif word == "python":
+            return "Best language ever!"
+        else:
+            return wiki.summary(word)
+    except:
+        return "Not Found"
+
+
+def text_pro(bot, update):
+    msg = update.message.text.lower()
+    sender_name = update.message.from_user.first_name
+    chat_id = update.message.chat.id
+    print("{}: {}".format(sender_name, msg))
+    if msg.startswith('wiki'):
+        bot.send_message(chat_id=chat_id, text=get_wiki(msg[5:]))
+        print("Bot: Wikipedia summery of {}".format(msg[5:]))
+    else:
+        bot.send_message(chat_id=chat_id, text="{}, Invalid command".format(sender_name))
+        print("Bot: Invalid command")
+
+
+updater.dispatcher.add_handler(MessageHandler(Filters.text, text_pro))
+updater.start_polling()
+print("Bot Server Started")
+updater.idle()
