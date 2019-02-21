8fef83a820b86b010101b49dc4403ade9ef4bb62
LSikyr <46223477+LSikyr@users.noreply.github.com>
2019-02-21 18:21:00 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,14 +2,13 @@ from telegram.ext import Updater
 from telegram.ext import MessageHandler, Filters
 import wikipedia as wiki
 
-
+# Bot Token is given by BotFather on Telegram while creating bot.
 TOKEN = "719781449:AAFPwK2uOz_OEWQCOFVaSAtLMUGphwUCVuk"
-
-updater = Updater(TOKEN)  #Bot Token is given by BotFather on Telegram while creating bot.
+updater = Updater(TOKEN)
 
 
 def get_wiki(word):
-    language_list = ["java", "js", "c#"]
+    language_list = ["java", "js", "c#", "c++", ""]
     try:
         if word in language_list:
             return "Python is better!"
@@ -30,11 +29,11 @@ def text_pro(bot, update):
         bot.send_message(chat_id=chat_id, text=get_wiki(msg[5:]))
         print("Bot: Wikipedia summery of {}".format(msg[5:]))
     else:
-        bot.send_message(chat_id=chat_id, text="{}, Invalid command".format(sender_name))
+        bot.send_message(chat_id=chat_id, text="Invalid command")
         print("Bot: Invalid command")
 
 
 updater.dispatcher.add_handler(MessageHandler(Filters.text, text_pro))
 updater.start_polling()
-print("Bot Server Started")
+print("server running...")
 updater.idle()
